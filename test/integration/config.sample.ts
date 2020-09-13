9d8759e7d1f8f30d10faf4718d0fb529d044e452
dshilkret <dshilkret@gmail.com>
2020-09-12 23:30:01 -0500

    update config

--- test/integration/config.sample.ts
+++ test/integration/config.sample.ts
@@ -8,6 +8,7 @@ import {
 } from './../../src/auth/IAuthOptions';
 
 export const onlineUrl = '[sharepoint online url]';
+export const onpremAddin ='https://spwzb27rov2c3sm.eastus2.cloudapp.azure.com';
 export const onpremAdfsEnabledUrl = '[sharepint on premise url with adfs configured]';
 export const onpremNtlmEnabledUrl = '[sharepint on premise url with ntlm]';
 export const onpremFbaEnabledUrl = '[sharepint on premise url with fba auth]';
@@ -45,11 +46,11 @@ export const onpremFbaCreds: IOnpremiseFbaCredentials = {
 };
 
 export const onpremAddinOnly: IOnPremiseAddinCredentials = {
-  clientId: '[clientId]',
-  issuerId: '[issuerId]',
-  realm: '[realm]',
-  rsaPrivateKeyPath: '[rsaPrivateKeyPath]',
-  shaThumbprint: '[shaThumbprint]'
+  clientId: 'b66f7e77-de3e-45d2-ba4c-b1b6405ec214',
+  issuerId: '9e9e46c4-6329-4990-a0b8-13b87b3ba56a',
+  realm: 'ddd67120-9259-451c-ad8f-b8cc3b28fac3',
+  rsaPrivateKeyPath: 'C:\\certs\\HighTrustOAuth.key',
+  shaThumbprint: 'QOCIyWwlxy8bM40Og6yzuj9vYkU'
 };
 
 export const onlineAddinOnly: IOnlineAddinCredentials = {
